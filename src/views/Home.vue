8d4f8330f06bf04a96bda509a4b6bc9ead2578c4
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-04-07 10:49:52 -0400

    create app and add index function using v-for

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+<!--     <p>Contacts : {{ contacts }}</p>
+ -->    
+ <div v-for="contact in contacts">
+      <p>First Name: {{contact.first_name}}</p>
+      <p>Middle Name: {{contact.middle_name}}</p>
+      <p>Last Name: {{contact.last_name}}</p>
+      <p>Phone Number: {{contact.phone_number}}</p>
+      <p>Email: {{contact.email}}</p>
+      <p>Bio: {{contact.bio}}</p>
+      <hr />
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "All Contacts",
+      contacts: []
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts").then(response => {
+      this.contacts = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
